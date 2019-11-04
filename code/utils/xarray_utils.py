fc2a3b808d09a53acadbfa838dc0cf2bc023f0fd
Mathias Hauser <mathias.hauser@env.ethz.ch>
2019-11-04 09:55:55 +0100

    add postprocess

--- code/utils/xarray_utils.py
+++ code/utils/xarray_utils.py
@@ -1,9 +1,56 @@
 import xarray as xr
 import numpy as np
 import glob
+import os
+from .file import _any_file_does_not_exist
+
+def postprocess(fN_out, fNs_in, metadata, transform_func=None, fixes=None, **kwargs):
+    """ postprocessing-on-the-fly and loading function
+
+    """
+
+    var = metadata.get('varn', None)
+
+    var_out = kwargs.pop('var_out', None)
+    dim = kwargs.pop('dim', "time")
+
+    if var_out is None:
+        var_out = var
+
+    # if fN_out does not exits, create it on the fly
+    if _any_file_does_not_exist(fN_out):
+
+        # postprocess
+        ds = read_netcdfs(
+            fNs_in,
+            dim=dim,
+            metadata=metadata,
+            transform_func=transform_func,
+            fixes=fixes,
+            **kwargs
+        )
+
+        if ds is None:
+            return []
+
+        if isinstance(ds, xr.DataArray):
+            ds = ds.to_dataset(name=var_out)
+
+        # add source files
+        ds.attrs["source_files"] = ", ".join(fNs_in)
+
+        # save as netcdf
+        ds.to_netcdf(fN_out, format="NETCDF4_CLASSIC")
+
+        return ds
+
+    else:
+        return xr.open_dataset(fN_out, use_cftime=True)
 
 
-def read_netcdfs(files, dim, transform_func=None, **kwargs):
+
+
+def read_netcdfs(files, dim, metadata, transform_func=None, fixes=None, **kwargs):
     """
     read and combine multiple netcdf files
 
@@ -38,6 +85,10 @@ def read_netcdfs(files, dim, transform_func=None, **kwargs):
 
         # use a context manager, to ensure the file gets closed after use
         with xr.open_dataset(path, **kwargs) as ds:
+
+            if fixes is not None:
+                ds = fixes(ds, metadata)
+
             # transform_func should do some sort of selection or
             # aggregation
             if transform_func is not None:
@@ -50,13 +101,13 @@ def read_netcdfs(files, dim, transform_func=None, **kwargs):
     kwargs["use_cftime"] = kwargs.pop("use_cftime", True)
 
     if isinstance(files, str):
-        paths = sorted(glob(files))
+        paths = sorted(glob.glob(files))
     else:
         paths = sorted(files)
 
     datasets = [process_one_path(p) for p in paths]
 
-    combined = xr.concat(datasets, dim, compat="override")
+    combined = xr.concat(datasets, dim, compat="override", coords="minimal")
 
     return combined
 
