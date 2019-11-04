24e4f88bb94120cc09d5a70356fd36eb69dc5421
Mathias Hauser <mathias.hauser@env.ethz.ch>
2019-11-04 15:59:57 +0100

    add product dict

--- code/filefinder/utils.py
+++ code/filefinder/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os as os
 import re
+import itertools
 
 
 def _find_keys(pattern):
@@ -35,3 +36,23 @@ def natural_keys(text):
 
     """
     return [atoi(c) for c in re.split(r"(\d+)", text)]
+
+
+def product_dict(**kwargs):
+    """ generate list of dictionaries with all possible combinations
+
+    https://stackoverflow.com/a/5228294/3010700
+
+    list(product_dict(a=[1, 2], b=[3, 4], c=[5]))
+
+    >>> [{'a': 1, 'b': 3, 'c': 5},
+         {'a': 1, 'b': 4, 'c': 5},
+         {'a': 2, 'b': 3, 'c': 5},
+         {'a': 2, 'b': 4, 'c': 5}]
+
+    """
+
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    for instance in itertools.product(*vals):
+        yield dict(zip(keys, instance))
