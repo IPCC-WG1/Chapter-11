3fb48d2df3a2023d7d6d4c47bdef0220c82d6e58
Mathias Hauser <mathias.hauser@env.ethz.ch>
2019-11-19 16:58:03 +0100

    add fixes for daily pr

--- code/fixes/_fixes_cmip5.py
+++ code/fixes/_fixes_cmip5.py
@@ -20,6 +20,17 @@ def cmip5_files(folder_in):
             # skip due to mess in files folder
             return None
 
+        if _corresponds_to(
+            metadata,
+            exp="piControl",
+            table="day",
+            varn="pr",
+            model="CMCC-CM",
+            ens="r1i1p1",
+        ):
+            # skip: uses mixed Gregorian/Julian calendar but goes over 1582-10-15
+            return None
+
         # =========================================================================
 
         # get the files in the directory
@@ -42,6 +53,21 @@ def cmip5_files(folder_in):
                 fNs_in, "tasmax_day_CMCC-CMS_rcp45_r1i1p1_20060101-20090930.nc"
             )
 
+        # some time period exists twice
+        if _corresponds_to(
+            metadata,
+            exp="piControl",
+            table="day",
+            varn="pr",
+            model="CMCC-CMS",
+            ens="r1i1p1",
+        ):
+            fNs_in = _maybe_remove_fN(
+                fNs_in,
+                "pr_day_CMCC-CMS_piControl_r1i1p1_38200101-38291231.nc",
+                "pr_day_CMCC-CMS_piControl_r1i1p1_38300101-38391231.nc",
+            )
+
         # some time periods after 2000 exists more than once
         if _corresponds_to(
             metadata,
@@ -52,10 +78,9 @@ def cmip5_files(folder_in):
             ens="r6i1p1",
         ):
             fNs_in = _maybe_remove_fN(
-                fNs_in, "pr_day_GISS-E2-H_historical_r6i1p1_20010101-20051231.nc"
-            )
-            fNs_in = _maybe_remove_fN(
-                fNs_in, "pr_day_GISS-E2-H_historical_r6i1p1_20000101-20121231.nc"
+                fNs_in,
+                "pr_day_GISS-E2-H_historical_r6i1p1_20010101-20051231.nc",
+                "pr_day_GISS-E2-H_historical_r6i1p1_20000101-20121231.nc",
             )
 
         # all after 2100 have a wrong time
@@ -87,11 +112,40 @@ def cmip5_files(folder_in):
             metadata,
             exp="piControl",
             table="day",
+            varn="pr",
+            model="HadGEM2-ES",
+            ens="r1i1p1",
+        ):
+            fNs_in = _maybe_remove_fN(
+                fNs_in, "pr_day_HadGEM2-ES_piControl_r1i1p1_20891201-20991110.nc"
+            )
+
+        # some time period exists twice
+        if _corresponds_to(
+            metadata,
+            exp="piControl",
+            table="day",
             varn="tasmax",
             model="IPSL-CM5B-LR",
             ens="r1i1p1",
         ):
             fNs_in = [fNs_in[0], fNs_in[4]]
+
+        # some time period exists twice
+        if _corresponds_to(
+            metadata,
+            exp="piControl",
+            table="day",
+            varn="pr",
+            model="IPSL-CM5B-LR",
+            ens="r1i1p1",
+        ):
+            fNs_in = _maybe_remove_fN(
+                fNs_in,
+                "pr_day_IPSL-CM5B-LR_piControl_r1i1p1_18300101-20291231.nc",
+                "pr_day_IPSL-CM5B-LR_piControl_r1i1p1_20300101-21291231.nc",
+                "pr_day_IPSL-CM5B-LR_piControl_r1i1p1_20800101-21291231.nc",
+            )
 
         return fNs_in
 
