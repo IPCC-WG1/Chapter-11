6f600d270a2d4d37e1c5a0c31e5b23edd5c47875
Mathias Hauser <mathias.hauser@env.ethz.ch>
2019-11-25 10:43:48 +0100

    compare cmip5 and cmip6

--- code/utils/plot.py
+++ code/utils/plot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def at_warming_level(
-    at_warming, unit, title, levels, average, mask_ocean=False, **kwargs
+    at_warming_c5, at_warming_c6, unit, title, levels, average, mask_ocean=False, **kwargs
 ):
 
     # determine function for averaging
@@ -18,7 +18,7 @@ def at_warming_level(
     else:
         raise "Not implemented"
 
-    f, axes = plt.subplots(2, 2, subplot_kw=dict(projection=ccrs.Robinson()))
+    f, axes = plt.subplots(3, 2, subplot_kw=dict(projection=ccrs.Robinson()))
 
     axes = axes.flatten()
 
@@ -31,10 +31,16 @@ def at_warming_level(
         **kwargs
     )
 
-    for i in range(4):
+    for i in range(3):
 
-        ax = axes[i]
-        d = ave_fnc(at_warming[i], "ens")
+        ax = axes[i * 2]
+        d = ave_fnc(at_warming_c5[i], "ens")
+        h = d.plot(ax=ax, **opt)
+
+    for i in range(3):
+
+        ax = axes[i * 2 + 1]
+        d = ave_fnc(at_warming_c6[i], "ens")
         h = d.plot(ax=ax, **opt)
 
         # add_sig_hatch_sign(ax, at_warming[i], thresh)
@@ -48,24 +54,34 @@ def at_warming_level(
             raise NotImplementedError()
 
     cbar = mpu.colorbar(
-        h, axes[2], axes[3], aspect=25, shrink=0.2, orientation="horizontal"
+        h, axes[-2], axes[-1], aspect=25, shrink=0.2, orientation="horizontal",
     )
     cbar.set_label(unit)
+    cbar.ax.tick_params(labelsize=10)
 
     titles = [
         "Tglob anomaly +1.5 °C",
         "Tglob anomaly +2.0 °C",
-        "Tglob anomaly +3.0 °C",
         "Tglob anomaly +4.0 °C",
     ]
 
-    for i, ax in enumerate(axes[:4]):
-        ax.set_title(titles[i], fontsize=8)
+    # for i, ax in enumerate(axes[:4]):
+    #     ax.set_title(titles[i], fontsize=8)
 
-    f.suptitle(title, fontsize=10, y=0.96)
+    mpu.ylabel_map("Tglob anomaly +1.5 °C", ax=axes[0], fontsize=9, labelpad=6)
+    mpu.ylabel_map("Tglob anomaly +2.0 °C", ax=axes[2], fontsize=9, labelpad=6)
+    mpu.ylabel_map("Tglob anomaly +4.0 °C", ax=axes[4], fontsize=9, labelpad=6)
 
-    plt.subplots_adjust(wspace=0.1, left=0.025, right=1 - 0.025, bottom=0.2, top=0.875)
+
+    axes[0].set_title("CMIP5", fontsize=11)
+    axes[1].set_title("CMIP6", fontsize=11)
+
+    f.suptitle(title, fontsize=11, y=0.975)
+
+    plt.subplots_adjust(wspace=0.075, left=0.05, right=1 - 0.025, bottom=0.15, top=0.9)
 
     mpu.set_map_layout(axes)
 
     f.canvas.draw()
+
+    return cbar
