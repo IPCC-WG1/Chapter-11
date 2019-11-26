5fb20caaf9a9d64dcffb0e2a0c2140931d5e2e8a
Mathias Hauser <mathias.hauser@env.ethz.ch>
2019-11-26 18:00:26 +0100

    only one file, add c6 gridinfo

--- code/warming_levels.py
+++ code/warming_levels.py
@@ -11,7 +11,9 @@ def warming_level_to_str(warming_level):
     return str(warming_level).replace(".", "")
 
 
-def warming_level_years(tas_list, warming_level, print_warming_level=True):
+def warming_level_years(
+    tas_list, warming_level, print_warming_level=True, add_grid_info=False
+):
 
     out = f"# warming level: {warming_level}°C above 1850-1900\n"
 
@@ -22,12 +24,17 @@ def warming_level_years(tas_list, warming_level, print_warming_level=True):
 
         beg, end, mid = computation.calc_year_of_warming_level(ds.tas, warming_level)
 
+        fmt = " {{model: {model}, ensemble: {ens}, exp: {exp}"
+
+        if add_grid_info:
+            fmt += ", grid: {grid}"
+
         if beg:
-            msg = "- {{model: {model}, ensemble: {ens}, exp: {exp}, start_year: {beg}, end_year: {end}}}\n"
-            out += msg.format(beg=beg, end=end, **metadata)
+            fmt = "-" + fmt + ", start_year: {beg}, end_year: {end}}}\n"
+            out += fmt.format(beg=beg, end=end, **metadata)
         else:
-            msg = "# {{model: {model}, ensemble: {ens}, exp: {exp}}} -- did not reach {warming_level}°C\n"
-            out += msg.format(warming_level=warming_level, **metadata)
+            fmt = "#" + fmt + "}} -- did not reach {warming_level}°C\n"
+            out += fmt.format(warming_level=warming_level, **metadata)
 
     out += "\n"
 
@@ -35,43 +42,49 @@ def warming_level_years(tas_list, warming_level, print_warming_level=True):
 
 
 def write_info(fid):
-    fid.write(f"# author: Mathias Hauser\n")
+    fid.write(f"# author: Mathias Hauser (mathias.hauser@env.ethz.ch)\n")
     fid.write(f"# script: warming_levels.py\n")
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S\n")
     fid.write(f"# executed: {now}\n")
 
 
-def write_warming_level_to_file(tas_list, conf_cmip):
+def write_warming_level_to_file(tas_list, conf_cmip, add_grid_info=False):
 
     # all warming levels in the same file
 
-    fN = conf_cmip.warming_levels_folder + "warming_levels_one_ens_all.yml"
+    if add_grid_info:
+        fN = conf_cmip.warming_levels_folder + "warming_levels_one_ens_grid.yml"
+    else:
+        fN = conf_cmip.warming_levels_folder + "warming_levels_one_ens.yml"
+
     with open(fN, "w") as fid:
         write_info(fid)
         for warming_level in WARMING_LEVELS:
-            string = warming_level_years(tas_list, warming_level)
+            string = warming_level_years(
+                tas_list, warming_level, add_grid_info=add_grid_info
+            )
             fid.write(string)
 
     # one file for all warming levels
 
-    for warming_level in WARMING_LEVELS:
+    # for warming_level in WARMING_LEVELS:
 
-        fN = conf_cmip.warming_levels_folder
-        fN += "warming_levels_one_ens_{}.yml".format(
-            warming_level_to_str(warming_level)
-        )
+    #     fN = conf_cmip.warming_levels_folder
+    #     fN += "warming_levels_one_ens_{}.yml".format(
+    #         warming_level_to_str(warming_level)
+    #     )
 
-        with open(fN, "w") as fid:
-            write_info(fid)
-            string = warming_level_years(
-                tas_list, warming_level, print_warming_level=False
-            )
-            fid.write(string)
+    #     with open(fN, "w") as fid:
+    #         write_info(fid)
+    #         string = warming_level_years(
+    #             tas_list, warming_level, print_warming_level=False
+    #         )
+    #         fid.write(string)
 
+
+WARMING_LEVELS = [1.5, 2.0, 3.0, 4.0]
 
 if __name__ == "__main__":
-
-    WARMING_LEVELS = [1.5, 2.0, 3.0, 4.0]
 
     # load data
     c5_tas = conf.cmip5.load_postprocessed_all_concat(
@@ -86,3 +99,5 @@ if __name__ == "__main__":
     write_warming_level_to_file(c5_tas, conf.cmip5)
 
     write_warming_level_to_file(c6_tas, conf.cmip6)
+
+    write_warming_level_to_file(c6_tas, conf.cmip6, add_grid_info=True)
