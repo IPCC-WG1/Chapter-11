806686624b8acab91e5b0009047abd6f87cdcff1
Mathias Hauser <mathias.hauser@env.ethz.ch>
2020-12-03 14:54:57 +0100

    unify_coord_names & allow changing calendars

--- code/fixes/_fixes_common.py
+++ code/fixes/_fixes_common.py
@@ -1,3 +1,7 @@
+import cftime
+import xarray as xr
+
+
 def _remove_matching_fN(fNs, *files_to_remove):
 
     for file_to_remove in files_to_remove:
@@ -18,6 +22,70 @@ def _corresponds_to(metadata, **conditions):
             conditions[key] = [value]
 
     return all(metadata[key] in cond for key, cond in conditions.items())
+
+
+def _maybe_rename(ds, name, target, candidates):
+    """
+
+    Parameters
+    ==========
+    ds : xr.Dataset
+        Dataset whose dims and coords should be renamed
+    name : str
+        Name of the coord/ dim to potentally rename
+    target : str
+        Desired name
+    candidates : list of str
+        Names that should be changed
+
+    """
+
+    if name in candidates:
+        ds = ds.rename({name: target})
+    return ds
+
+
+def unify_coord_names(ds):
+
+    dims = set(ds.dims)
+    no_dim_coords = set(ds.coords) - dims
+
+    # no_dim_coords must be first
+    for no_dim_coord in no_dim_coords:
+        ds = _maybe_rename(ds, no_dim_coord, "longitude", ["lon", "nav_lon"])
+        ds = _maybe_rename(ds, no_dim_coord, "latitude", ["lat", "nav_lat"])
+
+    for dim in dims:
+        ds = _maybe_rename(ds, dim, "lon", ["x", "i", "ni", "xh", "nlon", "longitude"])
+        ds = _maybe_rename(ds, dim, "lat", ["y", "j", "nj", "yh", "nlat", "latitude"])
+
+    return ds
+
+
+def convert_time_to_proleptic_gregorian(ds, dim="time"):
+
+    time = ds.indexes["time"]
+
+    time = [
+        cftime.DatetimeProlepticGregorian(
+            t.year, t.month, t.day, t.hour, t.minute, t.second
+        )
+        for t in time
+    ]
+
+    time = xr.CFTimeIndex(time)
+
+    return ds.assign_coords({dim: time})
+
+
+def convert_time_to(ds, new_calendar, dim="time"):
+
+    # convert the time back to the original
+    num, units, old_calendar = xr.coding.times.encode_cf_datetime(ds[dim])
+
+    time = xr.coding.times.decode_cf_datetime(num, units, new_calendar, use_cftime=True)
+
+    return ds.assign_coords({dim: time})
 
 
 def fixes_common(ds):
@@ -42,6 +110,8 @@ def fixes_common(ds):
     # delete height
     if "height" in ds.variables:
         del ds["height"]
+
+    ds = unify_coord_names(ds)
 
     # delete latitude bounds (#g1)
     if "lat" in ds.dims and "bounds" in ds["lat"].attrs:
