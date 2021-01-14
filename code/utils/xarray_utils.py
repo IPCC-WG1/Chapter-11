ce3a720d965a54cc31c5d64f50ae74e98d8e274e
Mathias Hauser <mathias.hauser@env.ethz.ch>
2021-01-14 10:11:34 +0100

    clean up xarray_utils

--- code/utils/xarray_utils.py
+++ code/utils/xarray_utils.py
@@ -1,51 +1,5 @@
-import glob
-
 import numpy as np
 import xarray as xr
-
-from .file_utils import _any_file_does_not_exist
-
-
-def postprocess(fN_out, metadata, data_reader, transform_func=None, **kwargs):
-    """postprocessing-on-the-fly and loading function"""
-
-    var = metadata.get("varn", None)
-    var_out = kwargs.pop("var_out", None)
-
-    if var_out is None:
-        var_out = var
-
-    # if fN_out does not exits, create it on the fly
-    if _any_file_does_not_exist(fN_out):
-
-        print("Before data read", end="")
-        ds = data_reader(**metadata)
-        print(" ... done")
-        # except Exception as e:
-        #     print("ERROR\n\n\n")
-        #     print(str(e))
-        #     print("ERROR\n\n\n")
-        #     return []
-
-        if transform_func is not None:
-            print("Before transform", end="")
-            ds = transform_func(ds)
-            print(" ... done")
-
-        if len(ds) == 0:
-            return []
-
-        if isinstance(ds, xr.DataArray):
-            ds = ds.to_dataset(name=var_out)
-
-        # fix for: https://github.com/pydata/xarray/issues/3665
-        if "time" in ds:
-            ds.time.encoding.pop("_FillValue", None)
-
-        # save as netcdf
-        print("Before saving", end="", flush=True)
-        ds.to_netcdf(fN_out, format="NETCDF4_CLASSIC")
-        print(" ... done")
 
 
 def mf_read_netcdfs(
@@ -79,87 +33,18 @@ def mf_read_netcdfs(
     if fixes is not None:
         ds = fixes(ds, metadata)
 
+    check_time_year_simple(ds)
+
+    # float32 is not good enough...
+    if ds[varn].dtype == np.float32:
+        ds[varn] = ds[varn].astype(float)
+
         # add source files
 
     fNs_in = [fNs_in] if isinstance(fNs_in, str) else fNs_in
     ds.attrs["source_files"] = ", ".join(fNs_in)
 
     return ds
-
-
-def read_netcdfs(files, dim, metadata, transform_func=None, fixes=None, **kwargs):
-    """
-    read and combine multiple netcdf files
-
-    Parameters
-    ----------
-    files : string or list of files
-        path with wildchars or iterable of files
-    dim : string
-        dimension along which to combine, does not have to exist in
-        file (e.g. ensemble)
-    transform_func : function
-        function to apply for individual datasets, see example
-    kwargs : keyword arguments
-        passed to open_dataset
-
-    Returns
-    -------
-    combined : xarray Dataset
-        the combined xarray Dataset with transform_func applied
-
-    Example
-    -------
-    read_netcdfs('/path/*.nc', dim='ens',
-                 transform_func=lambda ds: ds.mean())
-
-    Reference
-    ---------
-    http://xarray.pydata.org/en/stable/io.html#combining-multiple-files
-    """
-
-    def process_one_path(path, next_path):
-
-        # use a context manager, to ensure the file gets closed after use
-        with xr.open_dataset(path, **kwargs) as ds:
-
-            if fixes is not None:
-                ds = fixes(ds, metadata, next_path)
-
-            if ds is None:
-                return None
-
-            # transform_func should do some sort of selection or
-            # aggregation
-            if transform_func is not None:
-                ds = transform_func(ds)
-
-            # load all data from the transformed dataset, to ensure we can
-            # use it after closing each original file
-            ds.load()
-            return ds
-
-    kwargs["use_cftime"] = kwargs.pop("use_cftime", True)
-
-    if isinstance(files, str):
-        paths = sorted(glob.glob(files))
-    else:
-        paths = sorted(files)
-
-    datasets = list()
-
-    for i, path in enumerate(paths):
-        # get the next path in line (if there is one)
-        next_path = None if i + 1 == len(paths) else paths[i + 1]
-
-        ds = process_one_path(paths[i], next_path)
-
-        if ds is not None:
-            datasets.append(ds)
-
-    combined = xr.concat(datasets, dim, compat="override", coords="minimal")
-
-    return combined
 
 
 # =============================================================================
@@ -171,3 +56,34 @@ def cos_wgt(ds, lat="lat"):
 
 
 # =============================================================================
+
+def check_time_year_simple(ds, dim="time"):
+    # check that all years are present
+
+    if dim in ds.coords:
+        year = ds[dim].dt.year
+        first_year = year.min().item()
+        last_year = year.max().item()
+        
+        n_years_expected = last_year - first_year + 1
+        n_years_actual = len(np.unique(year))
+        
+        if not n_years_expected == n_years_actual:
+            raise ValueError("Missing years!")
+
+# =============================================================================
+
+
+def alignable(*objects):
+
+    try:
+        xr.align(*objects, join="exact", copy=False)
+        return True
+    except ValueError:
+        return False
+
+
+def assert_alignable(*objects, message=""):
+
+    if not alignable(*objects):
+        raise ValueError(message)
