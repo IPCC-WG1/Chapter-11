e69ad0720b630098fc4bf8104d7b758183d292e8
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-01-17 17:47:25 +0100

    license for utils/transform/_cdo.py

--- code/utils/transform/_cdo.py
+++ code/utils/transform/_cdo.py
@@ -1,15 +1,11 @@
-# import xesmf
-# import xarray
+# code vendored from cmip6-ng under the conditions of their license
+# https://git.iac.ethz.ch/cmip6-ng/cmip6-ng
+# see licenses/CMIP6_NG_LICENSE
+
 import logging
 import os
 
 import cdo
-
-from ..file_utils import _any_file_does_not_exist, _file_exists
-
-# import subprocess
-# import numpy as np
-
 
 logger = logging.getLogger(__name__)
 
@@ -29,13 +25,7 @@ def delete_corrupt_files(filename):
     try:
         CDO.info(filename)
     except cdo.CDOException:
-        logmsg = "\n".join(
-            [
-                "Deleting corrupt file:",
-                f"  {filename}",
-                "  Re-run script to create it again",
-            ]
-        )
+        logmsg = f"Deleting corrupt file: {filename}\nRe-run script to create it again"
         logger.warning(logmsg)
         os.remove(filename)
         return True
@@ -51,6 +41,23 @@ def _regrid_cdo(fN_in, fN_out, target_grid, method):
 def regrid_cdo(
     fN_in, fN_out, target_grid, overwrite=False, method="con2", cdo_version="1.9.8"
 ):
+    """regrid a netCDF file using cdo remap*
+
+    Parameters
+    ----------
+    fN_in : str
+        File name of netCDF file to regrid.
+    fN_out : str
+        Name to save the regridded file under.
+    target_grid : str
+        Name of target_grid. Grid definition must reside in ../grids/
+    overwrite : bool, default False
+        Overwrites fN_out if it already exists.
+    method : str, default: "con2"
+        Method to use for regridding.
+    cdo_version : str, optional
+        Only this version of cdo is allowed to be used.
+    """
 
     if isinstance(cdo_version, str):
         cdo_version = [cdo_version]
@@ -59,17 +66,16 @@ def regrid_cdo(
         logger.error(errmsg)
         raise ValueError(errmsg)
 
-    if overwrite and _file_exists(fN_out):
+    if overwrite and os.path.isfile(fN_out):
         os.remove(fN_out)  # delete old file to avoid permission errors
 
-    if overwrite or _any_file_does_not_exist(fN_out):
+    if overwrite or not os.path.isfile(fN_out):
 
         logger.info(f"Process target file:\n  {fN_out}")
         try:
             _regrid_cdo(fN_in, fN_out, target_grid, method)
         except cdo.CDOException:
-            if _file_exists(fN_out):
+            if os.path.isfile(fN_out):
                 os.remove(fN_out)  # make sure no broken file is left
             logger.error(f"cdo error! Regridding failed for source:\n  {fN_out}")
             delete_corrupt_files(fN_out)
-            return None
